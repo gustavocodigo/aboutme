35c99825635b31640a27cfadee37b8b1405f1136
gustavocodigo <gilapesgus@gmail.com>
2023-08-27 02:08:47 -0300

    initial commit

---
+++ js/nav.js
@@ -0,0 +1,67 @@
+const navId = document.getElementById("nav_menu"),
+  ToggleBtnId = document.getElementById("toggle_btn"),
+  CloseBtnId = document.getElementById("close_btn");
+
+// ==== SHOW MENU ==== //
+ToggleBtnId.addEventListener("click", () => {
+  navId.classList.add("show");
+});
+
+// ==== HIDE MENU ==== //
+CloseBtnId.addEventListener("click", () => {
+  navId.classList.remove("show");
+});
+
+// ==== Animate on Scroll Initialize  ==== //
+AOS.init();
+
+// ==== GSAP Animations ==== //
+// ==== LOGO  ==== //
+gsap.from(".profile", {
+  opacity: 0,
+  delay: 2,
+  duration: 0.1,
+});
+// ==== NAV-MENU ==== //
+gsap.from(".nav_menu_list .nav_menu_item", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+  stagger: 0.3,
+});
+// ==== TOGGLE BTN ==== //
+gsap.from(".toggle_btn", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+});
+// ==== MAIN HEADING  ==== //
+gsap.from(".main-heading", {
+  opacity: 0,
+  y: 20,
+  delay: 2.4,
+  duration: 1,
+});
+// ==== INFO TEXT ==== //
+gsap.from(".info-text", {
+  opacity: 0,
+  y: 20,
+  delay: 1.8,
+  duration: 1,
+});
+// ==== CTA BUTTONS ==== //
+gsap.from(".btn_wrapper", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 0.2,
+});
+// ==== TEAM IMAGE ==== //
+gsap.from(".team_img_wrapper img", {
+  opacity: 0,
+  y: 20,
+  delay: 3,
+  duration: 0.2,
+});
---
+++ js/script.js
@@ -0,0 +1,333 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const github_username = "gustavocodigo"
+
+    document.getElementById("topavatar").src = "https://github.com/" + github_username + ".png"
+
+    function updateInterceptors() {
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    }
+
+
+
+    document.addEventListener('scroll', function () {
+        // Aqui dentro a gente escreve o código que quer executar quando tiver o scroll
+        // Pode ser qualquer coisa, desde alterar o estilo de algum elemento até fazer uma requisição HTTP
+
+        // Por exemplo, posso exibir uma mensagem no console:
+
+        let scrollTop = document.documentElement.scrollTop
+
+        if (scrollTop <= 80) {
+            let arrow1 = document.getElementById("arrow1")
+            arrow1.style.transform = "rotate(0deg)";
+            arrow1.parentElement.href = "#stacks"
+
+        }
+
+        if (scrollTop >= window.innerHeight - 200) {
+            let arrow1 = document.getElementById("arrow1")
+            arrow1.style.transform = "rotate(180deg)";
+            arrow1.parentElement.href = "#top";
+        }
+
+
+
+
+        function isScrollNoFinal() {
+            const alturaJanela = window.innerHeight || document.documentElement.clientHeight;
+            const alturaTotalPagina = document.documentElement.scrollHeight;
+            const posicaoAtualScroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+            const distanciaDoFinal = window.innerHeight * 0.2;
+
+            return posicaoAtualScroll + alturaJanela >= alturaTotalPagina - distanciaDoFinal;
+        }
+
+
+
+        if (isScrollNoFinal()) {
+            console.log("Está no topo da página")
+            let arrow1 = document.getElementById("arrow2")
+            arrow1.style.transform = "rotate(180deg)";
+            arrow1.parentElement.href = "#stacks"
+        } else {
+            let arrow1 = document.getElementById("arrow2")
+            arrow1.style.transform = "rotate(0deg)";
+            arrow1.parentElement.href = "#contact"
+        }
+
+
+    });
+    updateInterceptors()
+
+
+    let stacks = [
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+            "title": "CSS",
+            "description": "Cascading Style Sheets (CSS) é uma linguagem de folha de estilos usada para descrever a apresentação de um documento escrito em HTML."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+            "title": "HTML",
+            "description": "Hypertext Markup Language (HTML) é a linguagem de marcação padrão para criar páginas da web e aplicações web."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+            "title": "JavaScript",
+            "description": "JavaScript é uma linguagem de programação que permite páginas da web e aplicações web interativas."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+            "title": "React",
+            "description": "React é uma biblioteca JavaScript para construção de interfaces de usuário, frequentemente usada para criar aplicações de página única."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+            "title": "Tailwind CSS",
+            "description": "Tailwind CSS é um framework CSS de utilitários que ajuda a criar designs personalizados e responsivos."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg",
+            "title": "Node.js",
+            "description": "Node.js é um ambiente de tempo de execução que permite executar JavaScript no lado do servidor."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+            "title": "Linux",
+            "description": "Linux é um sistema operacional de código aberto usado por várias distribuições para criar sistemas operacionais completos."
+        },
+        {
+            "avatar": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+            "title": "C++",
+            "description": "C++ é uma linguagem de programação de propósito geral conhecida por sua eficiência e flexibilidade."
+        }
+    ]
+
+
+
+    let lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam voluptates esse itaque aspernatur temporibus distinctio, ipsum dignissimos modi, iste repellat, voluptatem officiis? Vero mollitia cupiditate quia debitis molestiae. Minus, ab."
+
+
+
+    document.getElementById("stacklist").innerHTML = stacks.map((e) => `<div class="w-full md:w-[216px]  px-4 py-1  lg:w-54 rounded mt-4">
+
+    <div class="bg-[#121212DD] text-white px-4 py-2 rounded text-sm"> ${e.title}</div>
+    <p class="text-xs mt-4 px-2">
+     ${e.description}
+    </p>
+   
+    </div>`).join("\n")
+
+
+
+    let stacks2 = [
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" title="CSS" description="Cascading Style Sheets (CSS) é uma linguagem de folha de estilos usada para descrever a apresentação de um documento escrito em HTML."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" title="HTML" description="Hypertext Markup Language (HTML) é a linguagem de marcação padrão para criar páginas da web e aplicações web."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" title="JavaScript" description="JavaScript é uma linguagem de programação que permite páginas da web e aplicações web interativas."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" title="React" description="React é uma biblioteca JavaScript para construção de interfaces de usuário, frequentemente usada para criar aplicações de página única."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" title="Tailwind CSS" description="Tailwind CSS é um framework CSS de utilitários que ajuda a criar designs personalizados e responsivos."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" title="Node.js" description="Node.js é um ambiente de tempo de execução que permite executar JavaScript no lado do servidor."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" title="Linux" description="Linux é um sistema operacional de código aberto usado por várias distribuições para criar sistemas operacionais completos."/>`,
+
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" title="C++" description="C++ é uma linguagem de programação de propósito geral conhecida por sua eficiência e flexibilidade."/>`,
+        `<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" title="Figma" description="Figma é uma ferramenta de design baseada na web usada para criar interfaces de usuário, protótipos interativos e muito mais."/>`
+    ];
+
+
+
+    stacks2.forEach((e, i) => {
+        const div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.transform = "translate(-50%, -50%)";
+
+        const imgElement = document.createElement("img"); // Criar um elemento <img>
+        imgElement.src = e.match(/src="([^"]+)"/)[1]; // Extrair o valor do atributo src
+        imgElement.title = e.match(/title="([^"]+)"/)[1]; // Extrair o valor do atributo title
+        imgElement.setAttribute("description", e.match(/description="([^"]+)"/)[1]); // Extrair o valor do atributo description
+
+        const el = `
+            <div class="bg-[#121212DD] rounded text-xs absolute w-12 h-12 p-2 rounded-full hover:scale-125 cursor-pointer">
+                ${imgElement.outerHTML} <!-- Adicionar o elemento <img> -->
+            </div>`;
+
+        div.innerHTML = el;
+        document.getElementById("avatarback").appendChild(div);
+
+        const rad = (i / stacks2.length) * Math.PI * 2;
+        const w = div.parentElement.clientWidth;
+        const h = div.parentElement.clientHeight; // Corrigir aqui, alterar de clientWidth para clientHeight
+        const x = Math.cos(rad) * w * 0.36;
+        const y = Math.sin(rad) * h * 0.36;
+        div.style.top = (h / 2 + y) - h * 0.055 + "px";
+        div.style.left = (w / 2 + x) - w * 0.08 + "px";
+
+        div.addEventListener("click", () => {
+            const img = div.querySelector("img");
+            document.getElementById("description-title").innerText = img.title;
+            document.getElementById("description").innerText = img.getAttribute("description");
+            document.getElementById("infor").classList.remove("hidden")
+
+
+            gsap.from("#infor", {
+                opacity: 0,
+                delay: 0.1,
+                duration: 0.8,
+            });
+
+        });
+    });
+
+
+    updateInterceptors();
+
+
+
+
+
+
+    let projectsArray = [
+        {
+            link: "https://www.gptnerdes.com/",
+            name: "GptNerdes - GPT chat para nerdes"
+        },
+        {
+            link: "https://github.com/gustavocodigo/Youtube-downloader",
+            name: "Youtube Video Downloader"
+        },
+        {
+            link: "https://github.com/gustavocodigo/CodeLeap-test",
+            name: "CodeLeap CRUD Challeng"
+        },
+        {
+            link: "https://github.com/gustavocodigo/Academia-x1",
+            name: "Front de academia"
+        },
+        {
+            link: "https://github.com/gustavocodigo/NexusRP-reward1",
+            name: "Front de Loja (para aplicação desktop)"
+        },
+        {
+            link: "https://github.com/gustavocodigo/ForquilhaProgramador",
+            name: "Front de website para negocios"
+        },
+        {
+            link: "https://github.com/gustavocodigo/TicTacToeGame/",
+            name: "TicTac Toe Game - Jogo da velha"
+        },
+        {
+            link: "https://github.com/gustavocodigo/Pong-Game-ui-Canvas-Javascript/",
+            name: "PongGame - Jogo"
+        },
+        {
+            link: "https://github.com/gustavocodigo?tab=repositories",
+            name: "Meus repositorios"
+        }
+    ];
+
+    document.getElementById("projects").innerHTML = projectsArray.map((e) => `
+      <a href="${e.link}" class="text-xs whitespace-normal max-w-full" target="__blank">
+      <div class="mt-2 p-2 bg-[#323232] w-full md:w-[350px] max-w-full text-white rounded flex items-center space-x-4 duration-700 hover:bg-[#32323298] flex gap-2">
+      <img src="https://icons.iconarchive.com/icons/hopstarter/mac-folders-2/48/Folder-Mac-icon.png" width=28>
+       ${e.name}
+      </div>
+      </a>
+     
+    `).join("");
+
+
+
+
+    function enviarMensagemDiscord(webhookURL, contato, conteudo) {
+        return fetch(webhookURL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            content: `**Contato**: ${contato}\n\n**Conteúdo: **${conteudo}`
+          })
+        })
+        .then(response => {
+
+          if (response.status !== 204) {
+            throw new Error('Erro ao enviar mensagem para o Discord.');
+          }
+          return true
+        });
+      }
+
+
+    let contact = ""
+    let message = ""
+    let send_in = document.getElementById("send-in")
+    function update_send_button() {
+        if ((contact == "") || (message == "")) {
+            send_in.classList.add("bg-[#787878]")
+            send_in.classList.add("pointer-events-none")
+         
+
+        } else {
+            send_in.classList.remove("bg-[#787878]")
+            send_in.classList.remove("pointer-events-none")
+            send_in.classList.remove("bg-[#fc4830]")
+            
+
+        }
+        send_in.innerText = "ENVIAR"
+
+        send_in.classList.remove("bg-[#5eff71]")
+    }
+
+    document.getElementById("contact-in").addEventListener("keyup", function (ev) {
+        contact = ev.target.value
+        update_send_button()
+    })
+    document.getElementById("description-in").addEventListener("keyup", function (ev) {
+        message = ev.target.value
+        update_send_button()
+    })
+    update_send_button()
+
+
+    send_in.addEventListener("click", async function() {
+        document.getElementById("contact-in").disabled = true
+        document.getElementById("description-in").disabled = true
+        send_in.classList.add("bg-[#787878]")
+        send_in.classList.add("pointer-events-none")
+        send_in.innerText = "ENVIANDO..."
+
+
+        setTimeout(  async function(){
+            try {
+                await enviarMensagemDiscord("https://discord.com/api/webhooks/1145212969639219260/GCzb65_7udDONlJxisGEst8bWUWGb4EKluh3WkG7scTe6EheX6itsIttIgBQm2dG-cn9", contact, message)
+                send_in.classList.remove("bg-[#787878]")
+                send_in.classList.add("bg-[#5eff71]")
+                send_in.innerText = "ENVIADO COM SUCESSO !"
+                document.getElementById("contact-in").disabled = false
+                document.getElementById("description-in").disabled = false
+                send_in.classList.remove("pointer-events-none")
+                document.getElementById("description-in").value = ""
+                document.getElementById("description-in").placeholder = message
+                document.getElementById("contact-in").placeholder = contact
+                document.getElementById("contact-in").value = ""
+    
+            }catch(e) {
+                send_in.classList.remove("bg-[#787878]")
+                send_in.classList.add("bg-[#fc4830]")
+                send_in.innerText = "FALHA AO ENVIAR !"
+                document.getElementById("contact-in").disabled = false
+                document.getElementById("description-in").disabled = false
+            }
+        },2100)
+       
+    })
+
+})
